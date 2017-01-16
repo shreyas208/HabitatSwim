8134ef516b1976fb52a17b20d1d929a1346cb1f8
Shreyas Patil <shreyas208@gmail.com>
2017-01-16 10:58:09 +0200

    Added smtp_config file

---
+++ bin/smtp_config.php
@@ -0,0 +1,12 @@
+<?php
+
+$mail->isSMTP();
+$mail->Host = '';
+$mail->SMTPAuth = true;
+$mail->Username = '';
+$mail->Password = '';
+$mail->SMTPSecure = '';
+$mail->Port = 0;
+
+$mail->From = '';
+$mail->FromName = '';
